a510509bd5d90280d658778ce2b2d022145c2f4d
Manuel Nielsen <manuelnielsen.5g@gmail.com>
2021-09-23 04:37:34 -0300

    TP Conceptual 2

--- 4 Practico/Practico 6/js/formulario.js
+++ 4 Practico/Practico 6/js/formulario.js
@@ -13,43 +13,19 @@ const expresiones = {
 }
 
 const campos = {
-	//usuario: false,
-	//nombre: false,
 	calle: false,
 	numero: false,
-	ciudad: false,
-	//password: false,
-	//correo: false,	
-	//telefono: false
+	ciudad: false
 }
 
 const validarFormulario = (e) => {
 	switch (e.target.name) {
-		//case "usuario":
-		//	validarCampo(expresiones.usuario, e.target, 'usuario');
-		//break;
-		//case "nombre":
-		//	validarCampo(expresiones.nombre, e.target, 'nombre');
-		//break;
 		case "calle":
 			validarCampo(expresiones.calle, e.target, 'calle');
 		break;
 		case "numero":
 			validarCampo(expresiones.numero, e.target, 'numero');
 		break;				
-		//case "password":
-		//	validarCampo(expresiones.password, e.target, 'password');
-		//	validarPassword2();
-		//break;
-		//case "password2":
-		//	validarPassword2();
-		//break;
-		//case "correo":
-		//	validarCampo(expresiones.correo, e.target, 'correo');
-		//break;
-		//case "telefono":
-		//	validarCampo(expresiones.telefono, e.target, 'telefono');
-		//break;
 		case "ciudad":
 			validarCiudad();
 		break;
@@ -95,28 +71,6 @@ const validarCiudad =() =>{
 		campos[indexOf("ciudad")] = true;	
 	}
 }
-	/*const validarPassword2 = () => {
-	const inputPassword1 = document.getElementById('password');
-	const inputPassword2 = document.getElementById('password2');
-	if(inputPassword1.value !== inputPassword2.value)
-	{
-		document.getElementById(`grupo__password2`).classList.add('formulario__grupo-incorrecto');
-		document.getElementById(`grupo__password2`).classList.remove('formulario__grupo-correcto');
-		document.querySelector(`#grupo__password2 i`).classList.add('fa-times-circle');
-		document.querySelector(`#grupo__password2 i`).classList.remove('fa-check-circle');
-		document.querySelector(`#grupo__password2 .formulario__input-error`).classList.add('formulario__input-error-activo');
-		campos['password'] = false;
-		
-	} else 
-	{
-		document.getElementById(`grupo__password2`).classList.remove('formulario__grupo-incorrecto');
-		document.getElementById(`grupo__password2`).classList.add('formulario__grupo-correcto');
-		document.querySelector(`#grupo__password2 i`).classList.remove('fa-times-circle');
-		document.querySelector(`#grupo__password2 i`).classList.add('fa-check-circle');
-		document.querySelector(`#grupo__password2 .formulario__input-error`).classList.remove('formulario__input-error-activo');
-		campos['password'] = true;
-	}
-} */
 
 inputs.forEach((input) => {
 	input.addEventListener('keyup', validarFormulario);
@@ -146,16 +100,5 @@ function mostrarProductos(){
     document.getElementById('titulo-carrito').style.display = 'block';
 	
 }
-
-$( document ).ready(function() {
-    $('#vencimiento').bind('keyup','keydown', function(event) {
-    var inputLength = event.target.value.length;
-    if(inputLength === 2 || inputLength === 5){
-      var thisVal = event.target.value;
-      thisVal += '/';
-      $(event.target).val(thisVal);
-    }
-  })
-});
 
 
